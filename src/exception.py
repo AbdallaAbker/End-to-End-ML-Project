d2a277a77f5e640edd5c8745e9cbb97cdbb7e20d
aabdalla1088 <aabdalla1088@gmail.com>
2023-03-31 00:12:33 -0600

    data ingestion code

--- src/exception.py
+++ src/exception.py
@@ -5,10 +5,10 @@ def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+        file_name, exc_tb.tb_lineno, str(error))
     
     return error_message
-    )
+    
 
 
 class CustomException(Exception):
