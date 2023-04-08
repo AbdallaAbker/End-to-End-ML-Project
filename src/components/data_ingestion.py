b62523e303a252087e9fb123410ee0603dbf60ac
aabdalla1088 <aabdalla1088@gmail.com>
2023-04-07 23:32:26 -0600

    model training + hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainer
 
 #use decorator to define class variable 
 @dataclass
@@ -57,7 +57,9 @@ if __name__ == "__main__":
 
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
     
